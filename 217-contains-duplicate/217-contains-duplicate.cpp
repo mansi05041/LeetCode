0f6cfd4e3f16096bcf3825eab1fb7c9384cc5964
Mansi joshi <77066770+mansi05041@users.noreply.github.com>
2022-09-25 11:52:41 +0530

    Time: 116 ms (81.89%), Space: 53.5 MB (21.72%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+        for(auto x:freq){
+            if(x.second>1) return true;
+        }
+        return false;
+    }
+};
