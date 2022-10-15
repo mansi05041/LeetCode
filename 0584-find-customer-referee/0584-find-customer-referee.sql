db7946e037e4d07b07ba335e9104ea67e4ee2552
Mansi joshi <joshimansi32@gmail.com>
2022-10-15 14:25:34 +0530

    Time: 554 ms (62.16%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-select name from Customer
-where referee_id<>2 OR referee_id IS NULL;
+
+SELECT name 
+FROM Customer 
+WHERE referee_id<>2 OR referee_id IS NULL;
