4383bf99a70e0a6d995d74170d78cc8e3c225611
Mansi joshi <joshimansi32@gmail.com>
2022-10-18 08:34:28 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==1) return(1);
-        if(n==2) return(1);
+        if(n==1 || n==2) return(1);
         if(n>2){
             int first=0;
             int second=1;
