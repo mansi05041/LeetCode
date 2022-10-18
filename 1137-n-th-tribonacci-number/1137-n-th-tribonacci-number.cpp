815a5d86df3f485f9089e311aef32ea158b58dff
Mansi joshi <joshimansi32@gmail.com>
2022-10-18 08:54:44 +0530

    Time: 3 ms (35.69%), Space: 6 MB (52.74%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==1) return(1);
+        if(n==2) return(1);
+        if(n==3) return(2);
+        if(n>3){
+            unsigned int first=0;
+            unsigned int second=1;
+            unsigned int third=1;
+            unsigned int result;
+            while(n-2>0){
+                result=first+second+third;
+                first=second;
+                second=third;
+                third=result;
+                n-=1;
+            }
+            return(third);
+        }
+        return(0);
+    }   
+};
