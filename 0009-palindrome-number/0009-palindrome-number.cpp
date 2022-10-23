c76be4c171d93c2841cee61ad05d24bb55eabfd2
Mansi joshi <joshimansi32@gmail.com>
2022-10-23 21:32:00 +0530

    Time: 15 ms (86.96%), Space: 5.6 MB (100.00%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0||(x%10==0 && x!=0)) return false;
-        //if(x>=pow(-2,31) && x<=pow(2,31)-1) return false;
+        
         int n=x;
         long long palindrome=0;
         while(n){
             palindrome=palindrome*10+n%10;
             n/=10;
         }
-        //if(palindrome>=pow(-2,31) && palindrome<=pow(2,31)-1) return false;
+        
         return (x==palindrome);
     }
 };
