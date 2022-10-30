68cc681bd63deab10fac2052832707cdb6d423f2
Mansi joshi <joshimansi32@gmail.com>
2022-10-30 08:11:25 +0530

    Time: 479 ms (90.49%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -2,6 +2,6 @@
 select distinct l1.num as ConsecutiveNums
 from Logs l1
 join Logs l2
-on l1.id=l2.id+1 and l1.num=l2.num
+on l1.id=l2.id-1 and l1.num=l2.num
 join Logs l3
-on l2.id=l3.id+1 and l2.num=l3.num;
+on l2.id=l3.id-1 and l2.num=l3.num;
