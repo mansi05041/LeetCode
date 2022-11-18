42fdb519eb6925bd7611ded69cf5b7cc324e9a06
Mansi joshi <joshimansi32@gmail.com>
2022-11-18 07:26:25 +0530

    Time: 11 ms (35.58%), Space: 8.9 MB (11.15%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        // o(logn) - binary search
+        int l=0;
+        int r=nums.size()-1;
+        int mid;
+        while(l<r){
+            mid=l+(r-l)/2; 
+            if(nums[mid]>nums[mid+1]) r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
+};
