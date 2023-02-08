ac92f4d398825675016806cf872c5b43d5a5afd9
Mansi joshi <joshimansi32@gmail.com>
2023-02-08 21:41:36 +0530

    Update 0451-sort-characters-by-frequency.cpp

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -3,9 +3,11 @@ public:
     string frequencySort(string s) {
         priority_queue<pair<int,char>> pq;
         unordered_map<char,int> mp;
+        // store the character and frequency in map
         for(auto i:s){
             mp[i]++;
         }
+        // store the character according to the descending order in priority queue
         for(auto it: mp){
             pq.push({it.second,it.first});
         }
