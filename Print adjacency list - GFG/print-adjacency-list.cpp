43a2d122dd52e793b4b460770b4867e721165309
Mansi joshi <joshimansi32@gmail.com>
2023-04-21 20:24:33 +0530

    Added solution - LeetHub

---
+++ Print adjacency list - GFG/print-adjacency-list.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<vector<int>> graph(V);
+        // iterate over all the adjacency list
+        for(int i=0;i<V;i++){
+            // add the vertex itself
+            graph[i].push_back(i);
+            for(auto u:adj[i]){
+                // add the connected neighbours
+                graph[i].push_back(u);
+            }
+        }
+        return graph;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        vector<vector<int>> ans = obj.printGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            for (int j = 0; j < ans[i].size() - 1; j++) {
+                cout << ans[i][j] << "-> ";
+            }
+            cout << ans[i][ans[i].size() - 1];
+            cout << endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
