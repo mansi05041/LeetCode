6ebbf7b8d0779e110568949bb577c42e6bc13581
Mansi joshi <joshimansi32@gmail.com>
2023-05-20 12:43:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,13 +3,15 @@ public:
     int climbStairs(int n) {
         // base case
         if(n==0 || n==1) return 1;
-        // create dp vector 
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
+        int step1=1;
+        int step2=1;
+        int currStep;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            // either it takes one step or two steps
+            currStep = step1+step2;
+            step1 = step2;
+            step2 = currStep;
         }
-        return dp[n];
+        return step2;
     }
 };
