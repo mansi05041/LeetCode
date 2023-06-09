4ec929d1bb465aa7e575d9a950d1f69b64d5e241
Mansi joshi <joshimansi32@gmail.com>
2023-06-09 08:45:55 +0530

    Time: 11 ms (94.29%), Space: 15.8 MB (72.43%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for(auto it: letters){
-            if(it>target) return it;
+        // using binary search
+        int left=0, right=letters.size()-1, mid;
+        while(left<=right){
+            mid=(left+right)/2;
+            if(letters[mid]<=target) left = mid+1;
+            else right = mid-1;
         }
-        return letters[0];
+        return left==letters.size()? letters[0]: letters[left];
     }
 };
