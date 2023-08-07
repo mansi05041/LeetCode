6c0fc0c4abce3f4cd25ea059783f0835ce954b71
Mansi joshi <joshimansi32@gmail.com>
2023-08-07 22:03:01 +0530

    Time: 25 ms (98.65%), Space: 28.3 MB (24.08%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,23 +4,18 @@ public:
         
         if(nums.size()==0) return 0;
         
-        vector<int> sum(nums.size()+1);
-        sum[0] = nums[0];
+        int sum = 0;
         int minSize = INT_MAX;
-        
-        // calculate of sum of first n elements
-        for(int i=1;i<=nums.size();i++){
-            sum[i] = sum[i-1]+nums[i-1];
-        }
-        
-        for(int i=1;i<=nums.size();i++){
-            int findEle = target + sum[i-1];
-            auto bound = lower_bound(sum.begin(),sum.end(),findEle);
-            if(bound!=sum.end()){
-                minSize = min(minSize,static_cast<int>(bound-(sum.begin()+i-1)));
+        int start = 0;
+       
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            while(sum>=target){
+                minSize = min(minSize,i+1-start);
+                sum-=nums[start];
+                start++;
             }
         }
-        
         return minSize==INT_MAX ? 0 : minSize;
     }
 };
