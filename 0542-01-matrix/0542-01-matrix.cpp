7daf671b0a997087238e94bd6787ed5c73df7241
Mansi joshi <joshimansi32@gmail.com>
2023-08-17 07:02:59 +0530

    Time: 74 ms (74.95%), Space: 34.4 MB (25.75%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,12 +5,16 @@ public:
         int m = mat.size();
         int n = mat[0].size();
         
-        queue<pair<int,int>> q;
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        vector<vector<int>> dist(m,vector<int>(n));
+        queue<pair<pair<int,int>,int>> q;
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==0) q.push({i,j});
-                else mat[i][j]=-1;
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    visited[i][j]=1;
+                }
             }
         }
         
@@ -18,23 +22,26 @@ public:
         vector<int> col = {0,-1,1,0};
         
         while(!q.empty()){
-            auto r = q.front().first;
-            auto c = q.front().second;
+            auto r = q.front().first.first;
+            auto c = q.front().first.second;
+            auto d = q.front().second;
             q.pop();
+            
+            dist[r][c] = d;
             
             // explore all directions
             for(int i=0;i<4;i++){
                 int nr = r+row[i];
                 int nc = c+col[i];
                 
-                // if out of bound or having -1, then skip it
-                if(nr<0 || nc<0 || nr>=m || nc>=n || mat[nr][nc]!=-1) continue;
+                // if out of bound and already visited
+                if(nr<0 || nc<0 || nr>=m || nc>=n || visited[nr][nc]==1) continue;
                 
-                q.push({nr,nc});
-                mat[nr][nc]=mat[r][c]+1;
+                q.push({{nr,nc},d+1});
+                visited[nr][nc]=1;
             }
         }
         
-        return mat;
+        return dist;
     }
 };
